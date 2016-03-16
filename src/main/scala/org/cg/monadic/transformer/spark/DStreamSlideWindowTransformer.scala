8adfd53068ceffaa13530866e36099fcdeb72dfe
Yanlin Wang <wangyanlin@gmail.com>
2016-03-15 23:56:36 -0700

    change the moving window transformer structure

--- src/main/scala/org/cg/monadic/transformer/spark/DStreamSlideWindowTransformer.scala
+++ src/main/scala/org/cg/monadic/transformer/spark/DStreamSlideWindowTransformer.scala
@@ -13,38 +13,39 @@ import org.apache.spark.sql.Row
 import scala.util.Try
 
 /**
- * Spark moving window transformer 
- * 
+ * Spark moving window transformer
+ *
  * @author Yanlin Wang (wangyanlin@gmail.com)
  *
  */
 
-
-abstract class DStreamSlideWindowTransformer[IN <: Product : TypeTag] extends Transformer[Unit] {
+abstract class DStreamSlideWindowTransformer[IN <: Product: TypeTag] extends Transformer[Unit] {
   def inputStream: DStream[IN]
-  def name: String  //the name of sliding window and also the name of the in memory table 
+  def name: String //the name of sliding window and also the name of the in memory table 
   def sql: String // sql to do filter an/or aggregation
-  def windowSec : Int
-  def slideSec : Int
-  def handler : (String, Array[String], Array[Row]) => Unit
+  def windowSec: Int
+  def slideSec: Int
 
   override def transform() = {
     import scala.collection.JavaConverters._
 
-    val windowStream : DStream[IN] = inputStream.window(Seconds(windowSec), Seconds(slideSec))
-    windowStream.foreachRDD  { (rdd: RDD[IN], time : Time ) =>
+    val windowStream: DStream[IN] = inputStream.window(Seconds(windowSec), Seconds(slideSec))
+    windowStream.foreachRDD { (rdd: RDD[IN], time: Time) =>
       val sqlContext = SQLContext.getOrCreate(rdd.sparkContext)
       import sqlContext.implicits._
-      val df : DataFrame  = rdd.toDF()
+      val df: DataFrame = rdd.toDF()
       df.registerTempTable(name)
       val result = sqlContext.sql(sql)
-      val columns  = result.columns 
+      val columns = result.columns
       val data = result.collect()
-      try{
-        handler (name, columns, data)
+      try {
+        onData(name, columns, data)
       } catch {
-        case e: Throwable => logger.error(s"Error in handling sliding window $name", e) 
+        case e: Throwable => logger.error(s"Error in handling sliding window $name", e)
       }
     }
   }
+
+  def onData(name: String, columns: Array[String], data: Array[Row]): Unit
+
 }
